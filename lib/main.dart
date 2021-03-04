32e293e685767882d3c25eb5fbd261973bd8b1cf
Kevin Shah <kevin2252238@gmail.com>
2021-03-04 16:46:20 +0530

    Completed Readme

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Kevin\'s Drawing App',
       theme: ThemeData(
         primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
